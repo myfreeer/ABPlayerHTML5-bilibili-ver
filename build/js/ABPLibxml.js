421940eef41d3892cbb48d7fa746fa60ec9ae487
myfreeer <myfreeer@gmail.com>
2016-06-27 15:57:24 +0800

    Update ABPLibxml.js

--- build/js/ABPLibxml.js
+++ build/js/ABPLibxml.js
@@ -1,43 +1,45 @@
 /***********************
-* XMLParser
-* == Licensed Under the MIT License : /LICENSING
-* Copyright (c) 2012 Jim Chen ( CQZ, Jabbany )
-************************/
-function CommentLoader(url,xcm,callback){
-	if(callback == null)
-		callback = function(){return;};
-	var xmlhttp = null;
-	if (window.XMLHttpRequest){
-		xmlhttp=new XMLHttpRequest();
-	}
-	else{
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.open("GET",url,true);
-	xmlhttp.send();
-	var cm = xcm;
-	xmlhttp.onreadystatechange = function(){
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			if(navigator.appName == 'Microsoft Internet Explorer'){
-				var f = new ActiveXObject("Microsoft.XMLDOM");
-				f.async = false;
-				f.loadXML(xmlhttp.responseText);
-				cm.load(BilibiliParser(f));
-				callback();
-			}else{
-				var fffff=xmlhttp.responseXML==null?( new window.DOMParser() ).parseFromString(xmlhttp.responseText, "text/xml"):xmlhttp.responseXML;
-				cm.load(BilibiliParser(fffff));
-				//console.log(xmlhttp);
-				callback();
-			}
-		}
-	}
+ * XMLParser
+ * == Licensed Under the MIT License : /LICENSING
+ * Copyright (c) 2012 Jim Chen ( CQZ, Jabbany )
+ ************************/
+function CommentLoader(url, xcm, callback) {
+    if (callback == null)
+        callback = function() {
+            return;
+        };
+    var xmlhttp = null;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    var cm = xcm;
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            if (navigator.appName == 'Microsoft Internet Explorer') {
+                var f = new ActiveXObject("Microsoft.XMLDOM");
+                f.async = false;
+                f.loadXML(xmlhttp.responseText);
+                cm.load(BilibiliParser(f));
+                callback();
+            } else {
+                var parsedXML = xmlhttp.responseXML == null ? (new window.DOMParser()).parseFromString(xmlhttp.responseText, "text/xml") : xmlhttp.responseXML;
+                cm.load(BilibiliParser(parsedXML));
+                //console.log(xmlhttp);
+                callback();
+            }
+        }
+    }
 }
-function createCORSRequest(method, url){
+
+function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
+    if ("withCredentials" in xhr) {
         xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
+    } else if (typeof XDomainRequest != "undefined") {
         xhr = new XDomainRequest();
         xhr.open(method, url);
     } else {
